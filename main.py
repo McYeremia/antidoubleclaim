e2e14c712b08c684516e4669d36149fa9ac5aa29
McYeremia <yeremia.christopher@ti.ukdw.ac.id>
2026-02-26 21:24:44 +0700

    update code & requirements

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from text_similarity import token_sort_ratio
 
 text1 = "National Hackathon 2025 Juara 1"
-text2 = "Juara 1 National Hackathon 2025"
+text2 = "Juara 1 National Hackathon"
 
 score = token_sort_ratio(text1, text2)
 
 print("Token Sort Ratio:", score)
+
+from image_hash import generate_phash, hamming_distance
+
+hash1 = generate_phash("test_images/cert1.jpg")
+hash2 = generate_phash("test_images/cert2.jpg")
+
+distance = hamming_distance(hash1, hash2)
+
+print("Hamming Distance:", distance)
+
+if score >= 85:
+    print("Potensi Duplikat dari Teks")
+
+if distance <= 10:
+    print("Potensi Duplikat dari Sertifikat")
